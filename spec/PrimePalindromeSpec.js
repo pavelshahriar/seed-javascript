9e6320ba1224f2eb6ff049adf92a5c5020583fe7
Pavel Shahriar <shahriar.tanvir@gmail.com>
2015-03-19 20:12:39 -0500

    Prime number checking code integrated. Passed 2 test cases. Rest 4s are yet to be implemented

--- spec/PrimePalindromeSpec.js
+++ spec/PrimePalindromeSpec.js
@@ -6,27 +6,28 @@ describe("PrimePalindrome", function() {
     var prime_palindrome = new PrimePalindrome();
 
     it("should validate 5 to be a prime number!", function() {
-        expect( true ).toBe( prime_numbers.checkPrime(5) );
+        expect( true ).toBe( prime_palindrome.checkPrime(5) );
     });
 
     it("should invalidate 10 NOT to be a prime number!", function() {
-        expect( false ).toBe( prime_numbers.checkPrime(10) );
-    });
-
-    it("should return all prime numbers which are smaller than 1000!", function() {
-        expect( 1000 ).toBe( prime_numbers.getSmallerPrimeNumbersThan(1000).length );
+        expect( false ).toBe( prime_palindrome.checkPrime(10) );
     });
 
     it("should validate a palindrome", function() { // not necessarily it needs to be a prime number
-        expect( true ).toBe( prime_numbers.checkPalindrome('121'));
+        expect( true ).toBe( prime_palindrome.checkPalindrome('121'));
     });
 
     it("should invalidate a non-palindrome", function() { // not necessarily it needs to be a prime number
-        expect( true ).toBe( prime_numbers.checkPalindrome('122'));
+        expect( true ).toBe( prime_palindrome.checkPalindrome('122'));
+    });
+
+    // there are in total 168 primes under 1000. the formulea is - pi(1000) ~ 168
+    it("should return all prime numbers which are smaller than 1000!", function() {
+        expect(168).toBe( prime_palindrome.getSmallerPrimeNumbersThan(1000).length );
     });
 
     it("should return the largest prime palindrome smaller than 1000", function() { // not necessarily it needs to be a prime number
-        expect( 929 ).toBe( prime_numbers.getPrimePalindrome());
+        expect( 929 ).toBe( prime_palindrome.getPrimePalindrome());
     });
 
 });
--- src/PrimePalindrome.js
+++ src/PrimePalindrome.js
@@ -6,15 +6,21 @@ function PrimePalindrome() {
 }
 
 PrimePalindrome.prototype.checkPrime = function(num) {
-    // code for checking prime numbers goes here
-};
-
-PrimePalindrome.prototype.getSmallerPrimeNumbersThan = function(max_limit){
-    // code for getting all the prime numbers smaller than the supplied limit goes here
+    for(var i=2; i<=num/2; i++){ //start checking from 2, as all numbers are divisible by 1
+        if((num % i) === 0){
+            return false;
+        }
+    }
+    return true;
 };
 
 PrimePalindrome.prototype.checkPalindrome = function(num){
     // code for validating if a number is palindrome or not
+};
+
+PrimePalindrome.prototype.getSmallerPrimeNumbersThan = function(max_limit){
+    // code for getting all the prime numbers smaller than the supplied limit goes here
+    // dummy code now
 };
 
 PrimePalindrome.prototype.getPrimePalindrome = function(){
